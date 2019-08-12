299bf424c0a4e3290165b7f50f19d36a0192fcd9
Reza Akhlaqi <36783229+rzjrh@users.noreply.github.com>
2019-08-11 21:01:08 -0500

    Add files via upload

---
+++ CodeAssignment/src/main/java/com/RezaAk/web/CodeAssignment/CodeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.RezaAk.web.CodeAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeAssignmentApplication.class, args);
+	}
+
+}
